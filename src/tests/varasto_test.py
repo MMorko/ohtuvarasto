d24caa204d3abc6c471375bb48d08352e30c2b76
Atos Ikonen <atos.ikonen@gmail.com>
2025-11-03 20:43:49 +0200

    100% coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,42 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_laitetaan_liikaa_tavaraa(self):
+        self.varasto.lisaa_varastoon(12)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_otetaan_liikaa_tavaraa(self):
+        self.varasto.lisaa_varastoon(4)
+
+        saatu_maara = self.varasto.ota_varastosta(6)
+
+        self.assertAlmostEqual(saatu_maara, 4)
+
+    def test_lisataan_negatiivi_maara_tavaraa(self):
+        self.varasto.lisaa_varastoon(2)
+
+        self.varasto.lisaa_varastoon(-2)
+
+        self.assertAlmostEqual(self.varasto.saldo, 2)
+
+    def test_otetaan_negatiivi_maara_tavaraa(self):
+        self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_varaston_alkutilavuus_ei_negatiivinen(self):
+        var = Varasto(-2)
+        
+        self.assertAlmostEqual(var.tilavuus, 0)
+
+    def test_varaston_alku_saldo_ei_negatiivinen(self):
+        var = Varasto(10, -2)
+
+        self.assertAlmostEqual(var.saldo, 0)
+
+    def test_str_tulostus(self):
+        tulostus = str(self.varasto)
+
+        self.assertAlmostEqual(tulostus, "saldo = 0, vielä tilaa 10")
